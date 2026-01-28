56d71da8cdc7460cd59a1733b6416c4c12b213aa
Nanda Vikas Malireddy <nmalired@student.gitam.edu>
2026-01-28 10:59:04 +0530

    Create format_specifiers.py

---
+++ format_specifiers.py
@@ -0,0 +1,10 @@
+a = 25
+
+print(f"Decimal representation: {a}")
+print(f"Octal representation: {oct(a)}")
+print(f"Hexadecimal representation: {hex(a)}")
+
+#output
+Decimal representation: 25
+Octal representation: 0o31
+Hexadecimal representation: 0x19
