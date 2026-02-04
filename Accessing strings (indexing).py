04993622960ceaad7f90f339a079d1e69a5f76f8
Nanda Vikas Malireddy <nmalired@student.gitam.edu>
2026-02-04 11:42:17 +0530

    Create  Accessing strings (indexing).py

---
+++ Accessing strings (indexing).py
@@ -0,0 +1,10 @@
+s1 = "Nanda"
+s2 = 'Vikas'
+
+print("First character of s1:", s1[0])        # H
+print("Last character of s1:", s1[-1])        # o
+print()
+
+#output
+First character of s1: N
+Last character of s1: a
