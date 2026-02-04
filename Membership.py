ad7e96cfab164eb9ae5d841baeb558be318b98e9
Nanda Vikas Malireddy <nmalired@student.gitam.edu>
2026-02-04 11:54:01 +0530

    Add Membership.py with string membership checks

---
+++ Membership.py
@@ -0,0 +1,11 @@
+s1 = "Nanda"
+s2 = 'Vikas'
+s3 = """Malireddy Nanda Vikas"""
+
+print("'Py' in s2?", "Py" in s2)
+print("'java' not in s2?", "java" not in s2)
+print()
+
+#output
+'Py' in s2? False
+'java' not in s2? True
